ada5f00a2210c199f1a4d06b302f5106c949fdcc
kevin3251 <herry3251@gmail.com>
2020-06-10 17:43:25 +0800

    add TODO

--- util/dataframe.py
+++ util/dataframe.py
@@ -7,11 +7,16 @@ from graphene import ObjectType, String
 class DataFrame(ObjectType):
     __master = getenv('SPARK_MASTER', 'local')
 
-    def __init__(self):
+    def __init__(self, data):
         self.__spark = SparkSession.builder
             .master(__master)
             .appName(generate())
             .getOrCreate()
 
+        # TODO create dataframe
+        self.__df = spark.read.json()
+
     def __del__(self):
         self.__spark.stop()
+
+    
